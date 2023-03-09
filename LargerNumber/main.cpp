6e760326b035fd2a7f963e8caf4ef15144807764
sabiox100 <nathanielmuthomi4@gmail.com>
2023-03-09 23:11:14 +0300

    Add first solution

--- LargerNumber/main.cpp
+++ LargerNumber/main.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
 
+int main ()
+{
+    float x, y;
+    cout << "Enter first number: " ;
+    cin >> x;
+    cout << "\nEnter second number: ";
+    cin >> y;
+    if (x == y ) 
+    {
+        cout << "The two numbers are equal\n";
+    }
+    else if (x>y) 
+    {
+        cout << "The larger number is " << x;
+    }
+    else 
+    {
+        cout << "The larger number is " << y;
+    }
+    return 0;
+}
