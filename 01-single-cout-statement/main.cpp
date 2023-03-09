be423f456f6bec1079c1aa410aee68912315887f
sabiox100 <nathanielmuthomi4@gmail.com>
2023-03-09 22:41:19 +0300

    Add solution

--- 01-single-cout-statement/main.cpp
+++ 01-single-cout-statement/main.cpp
@@ -1,1 +1,9 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
 
+int main () 
+{
+    cout << setw(14) << "Maths = 90" << "\n" << setw(14) << "Physics = 77" << "\n" << "Chemistry = 69" << endl;
+    return 0;
+}
