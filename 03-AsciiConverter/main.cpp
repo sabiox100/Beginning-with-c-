a55a58d04098c1b5512f936458ea67c1d60a2ba6
sabiox100 <nathanielmuthomi4@gmail.com>
2023-03-09 23:27:39 +0300

    Add initial solutions.

--- 03-AsciiConverter/main.cpp
+++ 03-AsciiConverter/main.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+using namespace std;
 
+int main () 
+{
+    char c;
+    cout << "Enter character: " ;
+    cin >> c;
+    int asciiValue = (int)c;
+    cout << "The ASCII value of "<< c << " is " << asciiValue << "\n";
+    return 0;
+}
